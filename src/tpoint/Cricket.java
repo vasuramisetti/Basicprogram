c1c151069b29a2bb4e65d3953687406f7b61e624
vasudev <vasu123689@gmail.com>
2017-03-13 09:38:23 -0800

    Object creation

--- src/tpoint/Cricket.java
+++ src/tpoint/Cricket.java
@@ -1,11 +1,30 @@
 package tpoint;
-// basic program 
-
-
+// Creating object  
+//2nd program 
 public class Cricket {
 	
+	//variable decleration
+	
+	String name;
+	int age;
+	int totalruns;
+	String country;
+	Double totalOversPlayed;
+	
+	
+	
 	public static void main(String[] args) {
-	System.out.println("Hai i am a Cricket player");	
+		
+		// Creating object 
+	Cricket d = new Cricket();
+	d.age=34;
+	d.country="India";
+	d.name="Dhoni";
+	d.totalOversPlayed=4000.5;
+	d.totalruns=50000;	
+		
+	//printing Player details	
+	System.out.println("Here is player details:"+ "Name:" +d.name  +  "Age:" +  d.age  + "Country:" +d.country + "Total Overs Played:" + d.totalOversPlayed + "Total runs:"+ d.totalruns);	
 
 	}
 
