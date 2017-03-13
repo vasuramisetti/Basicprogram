3a02c73b8387e775444fb01973fde60ae20abd65
vasudev <vasu123689@gmail.com>
2017-03-13 10:24:51 -0800

    // Instance Variable (Variables Inside class But out side Method)

---
+++ src/tpoint/Cricket3.java
@@ -0,0 +1,11 @@
+package tpoint;
+// Instance Variable (Variables Inside class But out side Method)
+public class Cricket3 {
+
+	//variable decleration
+		String name;
+		int age;
+		int totalruns;
+		String country;
+		Double totalOversPlayed;
+}
---
+++ src/tpoint/Testcricket3.java
@@ -0,0 +1,33 @@
+package tpoint;
+
+public class Testcricket3{
+	// Instance Variable (Variables Inside class But out side Method)
+	
+public static void main(String[] args) {
+		
+		// Creating object 
+	Cricket d = new Cricket();
+	d.age=43;
+	d.country="India";
+	d.name="Sehwag";
+	d.totalOversPlayed=3000.5;
+	d.totalruns=20000;	
+	
+	
+	//creating second object
+	
+	Cricket d1 = new Cricket();
+	d1.age=24;
+	d1.country="India";
+	d1.name="Yuvi";
+	d1.totalOversPlayed=4000.7;
+	d1.totalruns=40000;
+		
+	//printing Player1 and 2 details	
+	System.out.println("Here is player1 details:"+"   "+ "Name:" +d.name +"   " +  "Age:" +  d.age +"   " + "Country:" +d.country+"   " + "TotalOversPlayed:" + d.totalOversPlayed +"   "+ "Total runs:"+ d.totalruns);	
+
+	System.out.println("Here is player2 details:" +"   "+ "Name:" +d1.name +"   " +  "Age:" +  d1.age +"   " + "Country:" +d1.country +"   "+ "TotalOversPlayed:"+ d1.totalOversPlayed +"   "+ "Total runs:"+ d1.totalruns);
+	}
+
+
+}
